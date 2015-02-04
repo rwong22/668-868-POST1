55a105473cd901beab128975f241b5cd6fbef796
Andee tao <andeetao@andees-mac-pro.attlocal.net>
2015-02-04 12:19:54 -0800

    added most of flow for store and some skeleton for post class

---
+++ src/Post.java
@@ -0,0 +1,23 @@
+public class Post {
+
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void cleanup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean verifyUPC(String scanned) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Item getItem(String scanned) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/Store.java
+++ src/Store.java
@@ -1,16 +1,83 @@
+import java.util.Scanner;
+
 //TODO: , , STORE, 
 //DONE: ITEM User, CUSTOMER
 public class Store {
+	static Scanner scanner = new Scanner(System.in);
+	static Customer customer;
+	static Post post = new Post();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String login;
+		do {
+			System.out.println("POST STATUS: Not Open");
+			System.out.print("hi, login: ");
+			login = scanner.nextLine();
+			System.out.println();
+		} while (!login.equals("admin"));
 
-		// open the store first
-		System.out.println("hi");
+		// admin stuff
+		System.out.println("POST STATUS: OPEN");
+		post.init();
 
-		// post mode
+		// POST mode:
+		do {
+			System.out.print("Login: ");
+			login = scanner.nextLine();
+			System.out.println();
+			if (!login.equals("admin")) {
+				customer = new Customer(login);
+				goToCustomerMode();
+			}
+
+		} while (!login.equals("admin"));
 
 		// close store
+		post.cleanup();
+		System.out.println("byebye");
+
 	}
 
+	private static void goToCustomerMode() {
+		// buy stuff
+		String scanned = "";
+		do {
+			System.out.print("hi, " + customer.getName()
+					+ " Enter UPC#, 'p' for payment, or 'x' for exit: ");
+			scanned = scanner.nextLine();
+			if (!scanned.equals("x") || !scanned.equals("p")) {
+				// check if upc is valid
+				if (post.verifyUPC(scanned)) {
+					// success
+					customer.addItem(post.getItem(scanned));// this might be a
+															// little odd, but
+															// will rework later
+															// on
+					System.out.println("UPC FOUND! item added to Cart");
+				} else {
+					// fail
+					System.out.println("sorry, UPC does not exsist in system.");
+				}
+			}
+			// payment mode
+			// if (scanned.equals("p")) {
+			// // check if upc is valid
+			// if (post.verifyUPC(scanned)) {
+			// // success
+			// customer.addItem(post.getItem(scanned));// this might be a
+			// // little odd, but
+			// // will rework later
+			// // on
+			// System.out.println("UPC FOUND! item added to Cart");
+			// } else {
+			// // fail
+			// System.out.println("sorry, UPC does not exsist in system.");
+			// }
+			// }
+
+		} while (!scanned.equals("x"));
+
+		// exit
+	}
 }
